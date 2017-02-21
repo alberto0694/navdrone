ca1f6314642a59311f8b39f8edc811c6872ae1b7
PC\Alberto <alberto.pimentel.94@gmail.com>
2017-02-21 02:20:36 -0300

    no message

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,14 @@ var $ = jQuery.noConflict();
 $(window).load(function () {
     "use strict";
 	$('#loader').fadeOut();
+    
+    $('.search-panel .dropdown-menu').find('a').click(function(e) {
+        e.preventDefault();
+        var param = $(this).attr("href").replace("#","");
+        var concept = $(this).text();
+        $('.search-panel span#search_concept').text(concept);
+        $('.input-group #search_param').val(param);
+    });    
 });
 
 
